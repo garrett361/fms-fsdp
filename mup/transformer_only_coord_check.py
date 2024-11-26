a050b8f94cbefa37b3fb202f0907d4e4aa872eb6
Garrett Goon <garrett361@gmail.com>
2024-11-26 18:55:29 +0000

    multiple seeds

--- mup/transformer_only_coord_check.py
+++ mup/transformer_only_coord_check.py
@@ -53,37 +53,43 @@ if __name__ == "__main__":
     parser.add_argument("--max_width", type=int, default=4096)
     parser.add_argument("--width_step", type=int, default=512)
     parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--num_seeds", type=int, default=2)
     args = parser.parse_args()
 
     results_list: list[dict] = []
     # Train repeatedly on fake data
-    torch.manual_seed(args.seed)
-    inputs_and_labels = torch.randint(
-        args.vocab_size, size=(1, args.seq_len + 1), device="cuda"
-    )
-    inputs = inputs_and_labels[:, :-1]
-    labels = inputs_and_labels[:, 1:]
-    for width in tqdm(
-        range(args.min_width, args.max_width + 1, args.width_step), desc="width"
-    ):
-        torch.manual_seed(args.seed)
-        model, config = get_transformer_and_config(width, vocab_size=args.vocab_size)
-        optimizer = torch.optim.AdamW(
-            model.parameters(), lr=args.lr, betas=(0.9, 0.95), weight_decay=0.1
+    for seed in tqdm(range(args.seed, args.seed + args.num_seeds), desc="seed"):
+        torch.manual_seed(seed)
+        inputs_and_labels = torch.randint(
+            args.vocab_size, size=(1, args.seq_len + 1), device="cuda"
         )
+        inputs = inputs_and_labels[:, :-1]
+        labels = inputs_and_labels[:, 1:]
+        for width in tqdm(
+            range(args.min_width, args.max_width + 1, args.width_step), desc="width"
+        ):
+            torch.manual_seed(seed)
+            model, config = get_transformer_and_config(
+                width, vocab_size=args.vocab_size
+            )
+            optimizer = torch.optim.AdamW(
+                model.parameters(), lr=args.lr, betas=(0.9, 0.95), weight_decay=0.1
+            )
 
-        get_stats(
-            model=model,
-            optimizer=optimizer,
-            train_steps=args.train_steps,
-            width=width,
-            inputs=inputs,
-            labels=labels,
-            results_list=results_list,
-        )
-        del model
-    df = pd.DataFrame(results_list)
-    parent_dir = Path(__file__).parent.absolute()
-    prefix = "transformer_only_coord_check"
-    df.to_feather(parent_dir.joinpath(f"{prefix}.feather"))
-    plot_from_df(df, save_path=f"{prefix}.png")
+            get_stats(
+                model=model,
+                optimizer=optimizer,
+                train_steps=args.train_steps,
+                width=width,
+                seed=seed,
+                seq_len=args.seq_len,
+                inputs=inputs,
+                labels=labels,
+                results_list=results_list,
+            )
+            del model
+        df = pd.DataFrame(results_list)
+        parent_dir = Path(__file__).parent.absolute()
+        prefix = "transformer_only_coord_check"
+        df.to_feather(parent_dir.joinpath(f"{prefix}.feather"))
+        plot_from_df(df, save_path=f"{prefix}.png")

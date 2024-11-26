5887f90948d90268f85e5715d879275ce53a511b
Garrett Goon <garrett361@gmail.com>
2024-11-26 20:13:01 +0000

    more mup init dev

--- mup/mup_mamba.py
+++ mup/mup_mamba.py
@@ -1,4 +1,14 @@
 from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+import torch.nn as nn
+from mamba_ssm.models.config_mamba import MambaConfig
+
+"""
+Basic mup implementation following Table 3 of 2203.03466. Specific to MambaLMHeadModel.
+"""
+
+
+def mup_cfg_check(cfg: MambaConfig) -> None:
+    pass
 
 
 def apply_mup_init(
@@ -9,6 +19,21 @@ def apply_mup_init(
     mup_attn_temp: float = 0,
     mup_lr_dscale: float = 0,
 ) -> None:
+    """
+    Apply mup init.
+
+    Notes:
+
+    mamba-ssm already does some init:
+
+    - https://github.com/state-spaces/mamba/blob/442fab4b1fd5226c1b5939b37d91ede430b5d1ae/mamba_ssm/models/mixer_seq_simple.py#L175-L182
+    - https://github.com/state-spaces/mamba/blob/442fab4b1fd5226c1b5939b37d91ede430b5d1ae/mamba_ssm/models/mixer_seq_simple.py#L86-L86
+
+    This does the following by default:
+    - Zeros out all Linear.bias terms except some which are important for mamba
+    - Forces the nn.Embedding layer to have 0.02 init
+    - Rescales some Linear weights following some prescription GPT-2 for residuals
+    """
     if not isinstance(model, MambaLMHeadModel):
         raise ValueError(
             f"mup only implemented for MambaLMHeadModel classes, not {model.__class__.__name__}"
@@ -24,3 +49,24 @@ def apply_mup_init(
     for layer_idx, block in enumerate(model.backbone.layers):
         print(f"Found {layer_idx=}: {block=}")
     assert model.lm_head is not None
+
+    cfg = model.config
+    mup_cfg_check(cfg)
+
+    for module in model.modules():
+        if isinstance(module, nn.Linear):
+            if module.bias is not None:
+                if not getattr(module.bias, "_no_reinit", False):
+                    nn.init.zeros_(module.bias)
+
+    # The embedding layer is an nn.Embedding which already performs unit-normal init and zeros out
+    # the padding entry, if needed.
+    embedding = model.backbone.embedding
+    # reset_parameters() perform unit-normal init and zeros out the padding entry, if applicable.
+    embedding.reset_parameters()
+
+    blocks = model.backbone.layers
+    for block in blocks:
+        pass
+    lm_head = model.lm_head
+    nn.init.normal_(lm_head.weight, mean=0.0, std=1 / cfg.d_model**0.5)

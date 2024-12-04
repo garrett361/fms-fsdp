58a476a506032096cdde24125064c637e2599a7a
Garrett Goon <garrett361@gmail.com>
2024-12-04 21:01:38 +0000

    wandb_test_min

---
+++ fms_fsdp/mup/wandb_test_min.py
@@ -0,0 +1,11 @@
+import wandb
+
+if __name__ == "__main__":
+    wandb.init(
+        project="goon-test",
+        dir=None,
+        resume="allow",
+        id="platform_test",
+    )
+    for idx in range(1, 11):
+        wandb.log(data={"loss": idx}, step=idx)

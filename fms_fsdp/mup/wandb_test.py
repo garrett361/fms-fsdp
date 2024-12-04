f5358737a3fadbe890fbd416fd1b2181afda7c5d
Garrett Goon <garrett361@gmail.com>
2024-12-04 21:19:43 +0000

    comment out more

--- fms_fsdp/mup/wandb_test.py
+++ fms_fsdp/mup/wandb_test.py
@@ -1,4 +1,3 @@
-import math
 import os
 from dataclasses import asdict
 
@@ -8,7 +7,6 @@ from pathlib import Path
 
 import fire
 import torch
-from fms_fsdp.utils.dataloader_utils import parse_data_args
 
 from fms_fsdp.utils.config_utils import update_config
 from fms_fsdp.utils.train_utils import (
@@ -20,12 +18,7 @@ from dataclasses import dataclass
 from fms_fsdp.utils.dataset_utils import (
     ArrowHandler,
     AutoHandler,
-    BufferDataset,
     ParquetHandler,
-    PreloadBufferDataset,
-    PreprocessDataset,
-    SamplingDataset,
-    StreamingDocDataset,
 )
 
 
@@ -104,112 +97,112 @@ class mup_config:
             raise ValueError("mup can only be specified along with a base_width")
 
 
-def causal_lm(data_seq, prompt_len=1):
-    """
-    Perform causal language modeling by right-shifting the input sequence.
-    Sets first prompt_len tokens to be ignored by the loss.
-    """
-    data_seq = data_seq.to(torch.int)
-    t = data_seq.clone()[1:]
-    data_seq = data_seq[:-1]
-    t[:prompt_len] = -100
-    return data_seq, t
+# def causal_lm(data_seq, prompt_len=1):
+#     """
+#     Perform causal language modeling by right-shifting the input sequence.
+#     Sets first prompt_len tokens to be ignored by the loss.
+#     """
+#     data_seq = data_seq.to(torch.int)
+#     t = data_seq.clone()[1:]
+#     data_seq = data_seq[:-1]
+#     t[:prompt_len] = -100
+#     return data_seq, t
 
 
-def get_data_loader(cfg, postprocess=[causal_lm]):
-    """
-    Pytorch dataloader for stateful, distributed, and rescalable causal language model (CLM) training.
-    Assumes underlying data is sequences of integer values.
-    ...
-    Args
-    ----
-    cfg : dataclass
-        Training config containing seq len, dataset, dataset weight, datapath, etc. arguments
-    postprocess : List[Callable]
-        Any task-specific postprocessing to apply before handing over data. Steps will apply in
-        the order provided by the user. For CLM training, use postprocess=[causal_lm].
-    """
+# def get_data_loader(cfg, postprocess=[causal_lm]):
+#     """
+#     Pytorch dataloader for stateful, distributed, and rescalable causal language model (CLM) training.
+#     Assumes underlying data is sequences of integer values.
+#     ...
+#     Args
+#     ----
+#     cfg : dataclass
+#         Training config containing seq len, dataset, dataset weight, datapath, etc. arguments
+#     postprocess : List[Callable]
+#         Any task-specific postprocessing to apply before handing over data. Steps will apply in
+#         the order provided by the user. For CLM training, use postprocess=[causal_lm].
+#     """
+#
+#     datasets, weights = parse_data_args(cfg.datasets, cfg.weights)
+#
+#     # Base streaming dataset. Returns doc chunks in sequence.
+#     # Implements dataset sampling and rescalability.
+#     droplist = [
+#         int(x.strip()) for x in cfg.strip_tokens.split(",") if len(x.strip()) > 0
+#     ]
+#     droplist = droplist + [cfg.bos_token, cfg.eos_token, cfg.bol_token, cfg.eol_token]
+#     assert (
+#         cfg.file_type in _handler_map
+#     ), f"File type {cfg.file_type} is not recognized ({list(_handler_map.keys())})"
+#     if cfg.file_type == "hf_parquet" or cfg.file_type == "auto":
+#         filehandler = _handler_map[cfg.file_type](cfg.tokenizer_path, cfg.col_name)
+#     else:
+#         filehandler = _handler_map[cfg.file_type]
+#     # Base reader layer
+#     rank, world_size = 0, 1
+#     data = StreamingDocDataset(
+#         cfg.data_path,
+#         rank,
+#         world_size,
+#         filehandler,
+#         cfg.eos_token,
+#         bos_token=cfg.bos_token,
+#         strip_tokens=set(droplist),
+#         min_length=3,
+#         seed=cfg.seed,
+#     )
+#     # Add multi-dataset handling
+#     data = SamplingDataset(
+#         cfg.data_path,
+#         data,
+#         cfg.eos_token,
+#         datasets=datasets,
+#         weights=weights,
+#         verbose=True,
+#     )
+#     # Wrap above dataset in packing logic to form constant-length lines.
+#     data = BufferDataset(
+#         data,
+#         cfg.seq_length if causal_lm not in postprocess else cfg.seq_length + 1,
+#         bos_token=cfg.bol_token,
+#         eos_token=cfg.eol_token,
+#         pack_hard=True,
+#     )
+#     # Shuffle outputs in length 10k buffer. Consecutive lines appear 10k steps apart on average.
+#     data = PreloadBufferDataset(data, 10000)
+#
+#     # Apply desired postprocessing steps in sequence
+#     data = PreprocessDataset(data, torch.IntTensor)
+#     for p in postprocess:
+#         data = PreprocessDataset(data, p)
+#
+#     return torch.utils.data.DataLoader(
+#         data, num_workers=cfg.num_workers, batch_size=cfg.batch_size
+#     )
 
-    datasets, weights = parse_data_args(cfg.datasets, cfg.weights)
 
-    # Base streaming dataset. Returns doc chunks in sequence.
-    # Implements dataset sampling and rescalability.
-    droplist = [
-        int(x.strip()) for x in cfg.strip_tokens.split(",") if len(x.strip()) > 0
-    ]
-    droplist = droplist + [cfg.bos_token, cfg.eos_token, cfg.bol_token, cfg.eol_token]
-    assert (
-        cfg.file_type in _handler_map
-    ), f"File type {cfg.file_type} is not recognized ({list(_handler_map.keys())})"
-    if cfg.file_type == "hf_parquet" or cfg.file_type == "auto":
-        filehandler = _handler_map[cfg.file_type](cfg.tokenizer_path, cfg.col_name)
-    else:
-        filehandler = _handler_map[cfg.file_type]
-    # Base reader layer
-    rank, world_size = 0, 1
-    data = StreamingDocDataset(
-        cfg.data_path,
-        rank,
-        world_size,
-        filehandler,
-        cfg.eos_token,
-        bos_token=cfg.bos_token,
-        strip_tokens=set(droplist),
-        min_length=3,
-        seed=cfg.seed,
-    )
-    # Add multi-dataset handling
-    data = SamplingDataset(
-        cfg.data_path,
-        data,
-        cfg.eos_token,
-        datasets=datasets,
-        weights=weights,
-        verbose=True,
-    )
-    # Wrap above dataset in packing logic to form constant-length lines.
-    data = BufferDataset(
-        data,
-        cfg.seq_length if causal_lm not in postprocess else cfg.seq_length + 1,
-        bos_token=cfg.bol_token,
-        eos_token=cfg.eol_token,
-        pack_hard=True,
-    )
-    # Shuffle outputs in length 10k buffer. Consecutive lines appear 10k steps apart on average.
-    data = PreloadBufferDataset(data, 10000)
-
-    # Apply desired postprocessing steps in sequence
-    data = PreprocessDataset(data, torch.IntTensor)
-    for p in postprocess:
-        data = PreprocessDataset(data, p)
-
-    return torch.utils.data.DataLoader(
-        data, num_workers=cfg.num_workers, batch_size=cfg.batch_size
-    )
-
-
-def get_dummy_loader(cfg):
-    """
-    A simple dummy dataloader yielding incrementing vocab indices in an infinite loop
-    """
-
-    class SteadyCounter(torch.utils.data.IterableDataset):
-        # Spit out incremental counts of constant length l, modulo vocab size v
-        def __init__(self, l, v):
-            self.i = 0
-            self.l = l
-            self.v = v
-
-        def __iter__(self):
-            while True:
-                out = torch.IntTensor(
-                    [x % self.v for x in range(self.i, self.i + self.l)]
-                )
-                yield out, out
-                self.i += self.l
-
-    data = SteadyCounter(cfg.seq_length, cfg.vocab_size)
-    return torch.utils.data.DataLoader(data, batch_size=cfg.batch_size)
+# def get_dummy_loader(cfg):
+#     """
+#     A simple dummy dataloader yielding incrementing vocab indices in an infinite loop
+#     """
+#
+#     class SteadyCounter(torch.utils.data.IterableDataset):
+#         # Spit out incremental counts of constant length l, modulo vocab size v
+#         def __init__(self, l, v):
+#             self.i = 0
+#             self.l = l
+#             self.v = v
+#
+#         def __iter__(self):
+#             while True:
+#                 out = torch.IntTensor(
+#                     [x % self.v for x in range(self.i, self.i + self.l)]
+#                 )
+#                 yield out, out
+#                 self.i += self.l
+#
+#     data = SteadyCounter(cfg.seq_length, cfg.vocab_size)
+#     return torch.utils.data.DataLoader(data, batch_size=cfg.batch_size)
 
 
 def train(
@@ -412,20 +405,20 @@ def main(**kwargs):
     # for g in optimizer.param_groups:
     #     g["initial_lr"] = cfg.learning_rate
 
-    # LR schedule  (cosine 0.01 decay)
-    warmup_interval = min(1000, cfg.num_steps // 10)
-    schedule = lambda x: min(
-        1 - (1 - min(x, warmup_interval) / warmup_interval) ** 2,
-        0.01
-        + 0.5
-        * (1 - 0.01)
-        * (1 + math.cos(min(x, cfg.num_steps) / cfg.num_steps * math.pi)),
-    )
-
-    # scheduler = LambdaLR(optimizer, lambda x: schedule(x))
-
-    # Train
-    print_device(f"Training for {cfg.num_steps} steps")
+    # # LR schedule  (cosine 0.01 decay)
+    # warmup_interval = min(1000, cfg.num_steps // 10)
+    # schedule = lambda x: min(
+    #     1 - (1 - min(x, warmup_interval) / warmup_interval) ** 2,
+    #     0.01
+    #     + 0.5
+    #     * (1 - 0.01)
+    #     * (1 + math.cos(min(x, cfg.num_steps) / cfg.num_steps * math.pi)),
+    # )
+    #
+    # # scheduler = LambdaLR(optimizer, lambda x: schedule(x))
+    #
+    # # Train
+    # print_device(f"Training for {cfg.num_steps} steps")
     train(
         cfg,
         None,

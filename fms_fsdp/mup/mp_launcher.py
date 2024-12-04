293af6d35f5c0dedeac29e5458b7dd12885df936
Garrett Goon <garrett361@gmail.com>
2024-12-04 15:29:35 +0000

    test mp launcher

--- fms_fsdp/mup/mp_launcher.py
+++ fms_fsdp/mup/mp_launcher.py
@@ -9,6 +9,8 @@ def target(rank: Union[str, int]) -> None:
 
 
 if __name__ == "__main__":
+    devices = [int(s) for s in os.environ["CUDA_VISIBLE_DEVICES"].split(",")]
+    print(devices)
     processes = [mp.Process(target=target, args=(rank,)) for rank in range(8)]
     for p in processes:
         p.start()

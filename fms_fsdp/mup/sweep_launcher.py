10a50f801873e9f57b6355143bf46971b2f6ff76
Garrett Goon <garrett361@gmail.com>
2024-12-05 15:49:47 +0000

    sweep test

--- fms_fsdp/mup/sweep_launcher.py
+++ fms_fsdp/mup/sweep_launcher.py
@@ -1,28 +1,40 @@
-import multiprocessing as mp
-import os
 from typing import Any
 
 import fire
-import wandb
 
-import traceback
-from single_gpu_training_transformer_only import get_cfg_from_kwargs, mup_config, main
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from single_gpu_training_transformer_only import mup_config
 
 
 """
-A wandb sweep launcher
-A pool based lr searcher.  Pass a --lrs="1e-3, 1e-4,..." arg along with other usual args to the cli
-and each lr will be created and run.
+A wandb sweep launcher. Pass a dict[str, tuple|list] --sweep_cfg arg which will be grid-scanned
+over.
 """
 
-SWEEP_CFG: dict[str, Any] = None
+SWEEP_CFG: dict[str, Any] = {
+    "method": "grid",
+    "metric": {"goal": "mnimize", "name": "loss"},
+}
 
 
 def populate_sweep_cfg(**kwargs) -> None:
     global SWEEP_CFG
     assert kwargs["tracker"] == "wandb"
+    # Expect a --sweep_cfg arg, which provides a dict
+    sweep_params = kwargs.pop("sweep_params")
+    if not isinstance(sweep_params, dict):
+        raise ValueError(f"Expected a dict, got {sweep_params=}")
+    # Merge in the sweep config w/ correct wandb syntax
+    for k, v in sweep_params.items():
+        if not isinstance(k, str) or not isinstance(v, (tuple, list)):
+            raise ValueError(
+                f"--sweep_params should be dict[str, tuple|list] dict, found {k=}, {v=}"
+            )
+        kwargs[k] = {"values": v}
+
+    # Merge the sweep config into the fixed config.
+
     SWEEP_CFG = kwargs
+    print(f"{SWEEP_CFG=}")
 
 
 def create_wandb_run_id(cfg: mup_config) -> str:
@@ -45,48 +57,48 @@ def create_wandb_run_id(cfg: mup_config) -> str:
 if __name__ == "__main__":
     fire.Fire(populate_sweep_cfg)
 
-    sweep_id = wandb.sweep(SWEEP_CFG, project=SWEEP_CFG["tracker_project_name"])
-
-    devices = [int(s) for s in os.environ["CUDA_VISIBLE_DEVICES"].split(",")]
-    num_devices = len(devices)
-    device_idx_queue = mp.Queue()
-    for device_idx in devices:
-        device_idx_queue.put(device_idx)
-
-    def main_wrapper(cfg_dict):
-        try:
-            device_idx = device_idx_queue.get()
-            os.environ["CUDA_VISIBLE_DEVICES"] = str(device_idx)
-            cfg = get_cfg_from_kwargs(**cfg_dict)
-
-            ctx = wandb.init(
-                project=cfg.tracker_project_name,
-                dir=cfg.tracker_dir,
-                resume="never",
-                id=None,
-                config=cfg_dict,
-            )
-            with ctx as run:
-                # Important: for some reason there are frequent hangs if we use a non-trivial id in
-                # wandb.init when this script is run under mutiprocessing, but it works fine if we
-                # just set the name by hand.
-                run.name = create_wandb_run_id(cfg)
-                res = main(cfg)
-            return (res, None, None)
-        except Exception as e:
-            return (None, e, traceback.format_exc())
-
-    # https://docs.wandb.ai/guides/track/log/distributed-training/#spawn-process
-    wandb.setup()
-
-    # Important to use ProcessPoolExecutor, and not Pool, because multiprocessing is used in the
-    # main function and Pool does not support nested mp.
-    with ProcessPoolExecutor(len(devices)) as executor:
-        futures = [
-            executor.submit(wandb.agent, sweep_id, main_wrapper) for _ in devices
-        ]
-        for f in as_completed(futures):
-            pass
-            # res, err, tb = f.result()
-            # if err:
-            #     print(f"Errored with {err=}\n{tb}")
+    # sweep_id = wandb.sweep(SWEEP_CFG, project=SWEEP_CFG["tracker_project_name"])
+    #
+    # devices = [int(s) for s in os.environ["CUDA_VISIBLE_DEVICES"].split(",")]
+    # num_devices = len(devices)
+    # device_idx_queue = mp.Queue()
+    # for device_idx in devices:
+    #     device_idx_queue.put(device_idx)
+    #
+    # def main_wrapper(cfg_dict):
+    #     try:
+    #         device_idx = device_idx_queue.get()
+    #         os.environ["CUDA_VISIBLE_DEVICES"] = str(device_idx)
+    #         cfg = get_cfg_from_kwargs(**cfg_dict)
+    #
+    #         ctx = wandb.init(
+    #             project=cfg.tracker_project_name,
+    #             dir=cfg.tracker_dir,
+    #             resume="never",
+    #             id=None,
+    #             config=cfg_dict,
+    #         )
+    #         with ctx as run:
+    #             # Important: for some reason there are frequent hangs if we use a non-trivial id in
+    #             # wandb.init when this script is run under mutiprocessing, but it works fine if we
+    #             # just set the name by hand.
+    #             run.name = create_wandb_run_id(cfg)
+    #             res = main(cfg)
+    #         return (res, None, None)
+    #     except Exception as e:
+    #         return (None, e, traceback.format_exc())
+    #
+    # # https://docs.wandb.ai/guides/track/log/distributed-training/#spawn-process
+    # wandb.setup()
+    #
+    # # Important to use ProcessPoolExecutor, and not Pool, because multiprocessing is used in the
+    # # main function and Pool does not support nested mp.
+    # with ProcessPoolExecutor(len(devices)) as executor:
+    #     futures = [
+    #         executor.submit(wandb.agent, sweep_id, main_wrapper) for _ in devices
+    #     ]
+    #     for f in as_completed(futures):
+    #         pass
+    #         # res, err, tb = f.result()
+    #         # if err:
+    #         #     print(f"Errored with {err=}\n{tb}")

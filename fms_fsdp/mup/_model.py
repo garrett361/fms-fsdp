d8427f370a5c4189eac861d8b1d7a2e95e2e7fc2
Garrett Goon <garrett361@gmail.com>
2024-12-11 04:05:11 +0000

    cfg cleanup and print check

--- fms_fsdp/mup/_model.py
+++ fms_fsdp/mup/_model.py
@@ -8,22 +8,25 @@ from fms_fsdp.mup._cfg import mup_config
 from fms_fsdp.mup._mup import _apply_mup_init
 
 
-def get_transformer(cfg: mup_config, device: str = "cuda") -> MambaLMHeadModel:
-    """
-    Creates a transformer-only model. Applies mup-init, if applicable.
-    """
-    # The mup paper discusses scaling up the attn_cfg.softmax_scale property, but that is only
-    # relevant if we are scaling up head_dim, which we are not currently doing.
-    # if cfg.mup:
-    #     cfg.attn_cfg["softmax_scale"] = cfg.head_dim
-
+def get_mamba_cfg_from_mup_cfg(cfg: mup_config) -> MambaConfig:
     filtered_kwargs = {
         k: v for k, v in asdict(cfg).items() if k in signature(MambaConfig).parameters
     }
     # Force the model to be transformer-only:
     filtered_kwargs["attn_layer_idx"] = list(range(cfg.n_layer))
+    return MambaConfig(**filtered_kwargs)
 
-    model = MambaLMHeadModel(MambaConfig(**filtered_kwargs), device=device)
+
+def get_transformer(cfg: mup_config, device: str = "cuda") -> MambaLMHeadModel:
+    """
+    Creates a transformer-only model. Applies mup-init, if applicable.
+    """
+    # The mup paper discusses scaling up the attn_cfg.softmax_scale property, but that only seems
+    # relevant if we are scaling up head_dim, which we are not currently doing.
+
+    mamba_cfg = get_mamba_cfg_from_mup_cfg(cfg)
+    print(f"Building model with {mamba_cfg=}")
+    model = MambaLMHeadModel(mamba_cfg, device=device)
     if cfg.mup:
         _apply_mup_init(model, cfg)
     return model

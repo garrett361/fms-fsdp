d767acf4ec39657d9ed7feed9b032381acdcb0b0
Garrett Goon <garrett361@gmail.com>
2025-03-14 10:58:12 -0400

    print loss, too

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -103,7 +103,7 @@ def train(
         loss.backward()
         # TODO: @goon - DELETE
         local_grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1e3)
-        print(f"[{batch_idx=}, {rank=}]: {local_grad_norm=}")
+        print(f"[{batch_idx=}, {rank=}]: {local_grad_norm=}, {loss=}")
 
         ddp_stats[1] += model.clip_grad_norm_(cfg.grad_clip_thresh).item()
         optimizer.step()

a1eebf0ebdbb18be34c8ccfb268712e4b544612f
Davis Wertheimer <dww78@cornell.edu>
2025-03-31 12:09:33 -0400

    Revert - need every rank to load back

--- fms_fsdp/utils/dataloader_utils.py
+++ fms_fsdp/utils/dataloader_utils.py
@@ -148,14 +148,13 @@ def get_data_loader(cfg, rank, world_size, dp_degree, postprocess=[causal_lm]):
         data = PreprocessDataset(data, lambda x: (chunk(x[0]), chunk(x[1])))
 
     # Enable auto-saving
-    if not do_cp or cp_rank==0:
-        data = CheckpointDataset(
-            data,
-            cfg.ckpt_load_path if cfg.resuming_dataset else cfg.ckpt_save_path,
-            cfg.checkpoint_interval,
-            cfg.batch_size,
-            cfg.ckpt_save_path,
-        )
+    data = CheckpointDataset(
+        data,
+        cfg.ckpt_load_path if cfg.resuming_dataset else cfg.ckpt_save_path,
+        cfg.checkpoint_interval,
+        cfg.batch_size,
+        cfg.ckpt_save_path,
+    )
     return torch.utils.data.DataLoader(
         data, num_workers=cfg.num_workers, batch_size=cfg.batch_size
     )

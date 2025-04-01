f084deb80d8dac233b46130ca143e7c587a5e2f0
Garrett Goon <garrett361@gmail.com>
2025-04-01 11:39:21 -0400

    remove "sharded" in hf ckpt util

--- fms_to_hf_mamba_transformers.py
+++ fms_to_hf_mamba_transformers.py
@@ -7,7 +7,7 @@ from transformers.models.bamba.convert_mamba_ssm_checkpoint import (
 def main(load_path, save_path, tokenizer_name_or_path):
     # NOTE: @goon - from Naigang
     convert_mamba_ssm_checkpoint_file_to_huggingface_model_file(
-        load_path, "fp32", save_path, tokenizer_name_or_path, "sharded"
+        load_path, "fp32", save_path, tokenizer_name_or_path
     )
 
 

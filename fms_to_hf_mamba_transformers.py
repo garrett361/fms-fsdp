d4c48e6cc9f65f37457214f4a6bae96214d92511
Garrett Goon <garrett361@gmail.com>
2025-04-01 11:10:13 -0400

    fms_to_hf_mamba_transformers.py

---
+++ fms_to_hf_mamba_transformers.py
@@ -0,0 +1,16 @@
+import fire
+from transformers.models.bamba.convert_mamba_ssm_checkpoint import (
+    convert_mamba_ssm_checkpoint_file_to_huggingface_model_file,
+)
+
+
+def main(load_path, save_path, tokenizer_name_or_path):
+    # NOTE: @goon - from Naigang
+    convert_mamba_ssm_checkpoint_file_to_huggingface_model_file(
+        load_path, "fp32", save_path, tokenizer_name_or_path, "sharded"
+    )
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
+

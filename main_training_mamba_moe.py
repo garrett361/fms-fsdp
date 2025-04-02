86665c63f3befe007dad7dfe1b4023bc7e9fb51b
Garrett Goon <garrett361@gmail.com>
2025-04-02 20:02:46 +0000

    start moe + ep training script

---
+++ main_training_mamba_moe.py
@@ -0,0 +1,205 @@
+import math
+import os
+from pathlib import Path
+
+import fire
+import torch
+import torch.optim as optim
+from mamba_ssm.models.config_mamba import MambaConfig
+from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+from mamba_ssm.modules.moe import MoE
+from torch import distributed as dist
+from torch.distributed import init_device_mesh
+from torch.distributed._composable.fsdp import fully_shard
+from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
+    checkpoint_wrapper,
+)
+from torch.distributed.fsdp import MixedPrecisionPolicy
+from torch.optim.lr_scheduler import LambdaLR
+
+from fms_fsdp import config
+from fms_fsdp.utils.checkpointing_utils import Checkpointer
+from fms_fsdp.utils.config_utils import get_model_config, update_config
+from fms_fsdp.utils.dataloader_utils import get_data_loader, get_dummy_loader
+from fms_fsdp.utils.train_utils import (
+    get_profiler,
+    setup,
+    setup_environ_flags,
+    train,
+)
+
+"""
+MoE + EP training. Requires torch nightly > 2.6. For use with the branch here: https://github.com/garrett361/mamba/pull/4
+"""
+
+
+def main(**kwargs):
+    # get configs
+    cfg = config.train_config()
+    update_config(cfg, **kwargs)
+
+    # ensure reproducibility
+    torch.cuda.manual_seed(cfg.seed)
+    torch.manual_seed(cfg.seed)
+
+    # torchrun specific
+    local_rank = int(os.environ["LOCAL_RANK"])
+    rank = int(os.environ["RANK"])
+    world_size = int(os.environ["WORLD_SIZE"])
+
+    if rank == 0:
+        print(f"--> running with these configs {cfg}")
+
+    # some setups
+    setup()
+    torch.cuda.set_device(local_rank)
+    torch.cuda.empty_cache()
+    setup_environ_flags()
+    os.environ["TRITON_CACHE_DIR"] = os.path.join(
+        Path.home(), ".triton", "cache", str(local_rank)
+    )
+
+    # get model
+    config_data = get_model_config(cfg.model_variant)
+    mamba_config = MambaConfig(**config_data)
+
+    # get data loader
+    if rank == 0:
+        print("Constructing datasets...")
+    if not cfg.use_dummy_dataset:
+        train_loader = get_data_loader(cfg, rank, world_size)
+    else:
+        train_loader = get_dummy_loader(cfg, rank, world_size)
+    if rank == 0:
+        print("Datasets constructed!")
+
+    # FSDP
+    if cfg.sharding_strategy == "hsdp":
+        raise NotImplementedError("Just full FSDP for now")
+    else:
+        mesh = init_device_mesh("cuda", (world_size,), mesh_dim_names=("dp_shard",))
+
+    model = MambaLMHeadModel(mamba_config, ep_mesh=mesh)
+    if rank == 0:
+        total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print(f"\n--> model has {total_params / 1e6} Million params\n")
+
+    # AC
+    if cfg.fsdp_activation_checkpointing:
+        for layer_index, block in enumerate(model.backbone.layers):
+            model.backbone.layers[layer_index] = checkpoint_wrapper(
+                block, preserve_rng_state=False
+            )
+
+    mp_policy = MixedPrecisionPolicy(
+        param_dtype=torch.bfloat16, reduce_dtype=torch.bfloat16
+    )
+    # Assumed: no tied params
+    fully_shard(model.lm_head, mesh=mesh, mp_policy=mp_policy)
+    fully_shard(model.backbone.embedding, mesh=mesh, mp_policy=mp_policy)
+    # NOTE: @goon - model.backbone.layers is a module_dict on the MoE branch
+    for idx, block in model.backbone.layers.items():
+        # The ignored_params arg requires torch nightly (> 2.6.0)
+        ignored_params = (
+            set(block.mlp.experts.parameters()) if isinstance(block.mlp, MoE) else None
+        )
+        is_not_last_block = int(idx) < len(model.backbone.layers) - 1
+        fully_shard(
+            block,
+            mesh=mesh,
+            ignored_params=ignored_params,
+            mp_policy=mp_policy,
+            reshard_after_forward=is_not_last_block,
+        )
+    fully_shard(model, mesh=mesh, reshard_after_forward=False, mp_policy=mp_policy)
+
+    if rank == 0:
+        print(f"{model=}")
+
+    # torch compile
+    if cfg.use_torch_compile:
+        if rank == 0:
+            print("--> enabling torch compile...")
+        # the default accumulated_cache_size_limit=64 is not enough for 70b model, so we make it 128 here
+        torch._dynamo.config.accumulated_cache_size_limit = 128
+        model = torch.compile(model)
+
+    # Optimizer
+    optimizer = optim.AdamW(
+        model.parameters(), lr=cfg.learning_rate, betas=(0.9, 0.95), weight_decay=0.1
+    )
+
+    # optionally load from checkpoint (when continue pretraining)
+    checkpointer = Checkpointer(
+        cfg.ckpt_save_path, 1000, cfg.sharding_strategy, rank, local_rank
+    )
+    model, optimizer, _, start_step, tokens_seen, is_resuming = checkpointer.load(
+        model,
+        optimizer,
+        None,
+        path=os.path.join(cfg.ckpt_load_path, "checkpoints/")
+        if not os.path.isfile(cfg.ckpt_load_path)
+        else cfg.ckpt_load_path,
+        strict=False,
+    )
+    if not is_resuming:
+        start_step = 0
+        # Override loaded optim hyperparams with the current values
+        for g in optimizer.param_groups:
+            g["initial_lr"] = cfg.learning_rate
+
+    # LR schedule
+    # linear decay for annealing
+    if cfg.training_stage == "annealing":
+        warmup_interval = 1000
+        schedule = (
+            lambda x: x / warmup_interval
+            if x < warmup_interval
+            else 1 - (x - warmup_interval) / (cfg.num_steps - warmup_interval)
+        )
+    elif cfg.training_stage == "cosine":
+        # cosine decay
+        warmup_interval = min(2000, cfg.num_steps // 20)
+        schedule = lambda x: min(
+            1 - (1 - min(x, warmup_interval) / warmup_interval) ** 2,
+            0.1
+            + 0.5
+            * (1 - 0.1)
+            * (1 + math.cos(min(x, cfg.num_steps) / cfg.num_steps * math.pi)),
+        )
+    elif cfg.training_stage == "constant":
+        warmup_interval = 2000
+        schedule = lambda x: (min(x, warmup_interval) / warmup_interval)
+    else:
+        schedule = lambda x: 1.0 + (0.75 - 1.0) * (x / 32000) if x <= 32000 else 0.75
+
+    scheduler = LambdaLR(optimizer, lambda x: schedule(x + start_step))
+
+    # profiler
+    profiler = get_profiler(cfg, rank)
+
+    # Train
+    if rank == 0:
+        print(f"Training for {cfg.num_steps} steps")
+    train(
+        cfg,
+        model,
+        local_rank,
+        rank,
+        train_loader,
+        optimizer,
+        scheduler,
+        profiler,
+        checkpointer,
+        start_step,
+        tokens_seen,
+    )
+
+    checkpointer.save_single_file(cfg.num_steps, model)
+
+    dist.barrier()
+    dist.destroy_process_group()
+
+
+if __name__ == "__main__":
+    fire.Fire(main)

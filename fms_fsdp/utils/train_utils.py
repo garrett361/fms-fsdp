6c18ff4cbd321687f94d9fb8f9426fabf8e7d750
Garrett Goon <garrett361@gmail.com>
2025-04-03 16:39:53 -0400

    print fix

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -89,7 +89,9 @@ def train(
         input = input.to(local_rank)
         label = label.to(local_rank)
         if cfg.sanity_print_toks:
-            print(f"[{rank=}]: inputs {tokenizer.decode(input.cpu().tolist())}")
+            toks_list = input.cpu().tolist()
+            for toks in toks_list:
+                print(f"[{rank=}]:  {tokenizer.decode(toks)}")
 
         optimizer.zero_grad()
         output = model(input)

74995fdd22845755cbca09982b9835840828fd01
Davis Wertheimer <dww78@cornell.edu>
2025-04-07 18:18:32 -0400

    Up slice rate

--- fms_fsdp/utils/dataloader_utils.py
+++ fms_fsdp/utils/dataloader_utils.py
@@ -140,6 +140,7 @@ def get_data_loader(cfg, rank, world_size, dp_degree, postprocess=[causal_lm]):
     data = DocSliceDataset(
         data,
         cfg.eos_token,
+        slice_rate=.75,
     )
 
     # Apply desired postprocessing steps in sequence

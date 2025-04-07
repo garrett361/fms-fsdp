ddb0a5a2911fca187a92f87d110fd919a0371c67
Garrett Goon <garrett361@gmail.com>
2025-04-07 19:28:02 +0000

    add more moe configs

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -248,6 +248,100 @@ def get_model_config(model_variant):
             "pad_vocab_size_multiple": 16,
             "tie_embeddings": False,
         }
+    # Translated from https://github.com/foundation-model-stack/fms-fsdp/compare/main...moe
+    elif model_variant == "mamba_30b_moe":
+        model_config = {
+            "d_model": 3072,
+            "d_intermediate": 1344,
+            "n_layer": 32,
+            "vocab_size": 128256,
+            "ssm_cfg": {"layer": "Mamba2"},
+            "attn_layer_idx": [9, 18, 27],
+            "attn_cfg": {
+                "causal": True,
+                "d_conv": 0,
+                "head_dim": 128,
+                "num_heads": 24,
+                "num_heads_kv": 8,
+                "out_proj_bias": False,
+                "qkv_proj_bias": False,
+                "rotary_emb_dim": 64,
+            },
+            "moe_layer_idx": list(range(32)),
+            "moe_cfg": {
+                "n_routed_experts": 64,
+                "n_activated_experts": 8,
+                "n_shared_experts": 0,
+                "hidden_features": 1344,
+            },
+            "rms_norm": True,
+            "residual_in_fp32": True,
+            "fused_add_norm": True,
+            "pad_vocab_size_multiple": 16,
+            "tie_embeddings": False,
+        }
+    elif model_variant == "mamba_120b_moe":
+        model_config = {
+            "d_model": 4096,
+            "d_intermediate": 896,
+            "n_layer": 40,
+            "vocab_size": 128256,
+            "ssm_cfg": {"layer": "Mamba2"},
+            "attn_layer_idx": [9, 18, 27, 36],
+            "attn_cfg": {
+                "causal": True,
+                "d_conv": 0,
+                "head_dim": 128,
+                "num_heads": 32,
+                "num_heads_kv": 8,
+                "out_proj_bias": False,
+                "qkv_proj_bias": False,
+                "rotary_emb_dim": 64,
+            },
+            "moe_layer_idx": list(range(40)),
+            "moe_cfg": {
+                "n_routed_experts": 256,
+                "n_activated_experts": 16,
+                "n_shared_experts": 0,
+                "hidden_features": 896,
+            },
+            "rms_norm": True,
+            "residual_in_fp32": True,
+            "fused_add_norm": True,
+            "pad_vocab_size_multiple": 16,
+            "tie_embeddings": False,
+        }
+    elif model_variant == "mamba_236b_moe":
+        model_config = {
+            "d_model": 5120,
+            "d_intermediate": 1536,
+            "n_layer": 60,
+            "vocab_size": 128256,
+            "ssm_cfg": {"layer": "Mamba2"},
+            "attn_layer_idx": [9, 18, 27, 36, 45, 54],
+            "attn_cfg": {
+                "causal": True,
+                "d_conv": 0,
+                "head_dim": 128,
+                "num_heads": 40,
+                "num_heads_kv": 8,
+                "out_proj_bias": False,
+                "qkv_proj_bias": False,
+                "rotary_emb_dim": 64,
+            },
+            "moe_layer_idx": list(range(40)),
+            "moe_cfg": {
+                "n_routed_experts": 160,
+                "n_activated_experts": 8,
+                "n_shared_experts": 0,
+                "hidden_features": 1536,
+            },
+            "rms_norm": True,
+            "residual_in_fp32": True,
+            "fused_add_norm": True,
+            "pad_vocab_size_multiple": 16,
+            "tie_embeddings": False,
+        }
     else:
         raise ValueError(f"model variant {model_variant} not supported.")
 

75e1d1f477b9809290899e74d32e86c9190e2bb1
Garrett Goon <garrett361@gmail.com>
2025-04-08 12:56:47 +0000

    more model param count tests

--- tests/moe/test_model.py
+++ tests/moe/test_model.py
@@ -21,3 +21,27 @@ class TestBuildModels:
             model = MambaLMHeadModel(mamba_config)
         params = sum(p.numel() for p in model.parameters())
         assert params == 2_516_267_712  # ~ 2B
+
+    def test_mamba_30b_moe(self) -> None:
+        config_data = get_model_config("mamba_30b_moe")
+        mamba_config = MambaConfig(**config_data)
+        with torch.device("meta"):
+            model = MambaLMHeadModel(mamba_config)
+        params = sum(p.numel() for p in model.parameters())
+        assert params == 27_911_687_584  # ~ 27B
+
+    def test_mamba_120b_moe(self) -> None:
+        config_data = get_model_config("mamba_120b_moe")
+        mamba_config = MambaConfig(**config_data)
+        with torch.device("meta"):
+            model = MambaLMHeadModel(mamba_config)
+        params = sum(p.numel() for p in model.parameters())
+        assert params == 117_685_942_784  # ~ 117B
+
+    def test_mamba_236b_moe(self) -> None:
+        config_data = get_model_config("mamba_236b_moe")
+        mamba_config = MambaConfig(**config_data)
+        with torch.device("meta"):
+            model = MambaLMHeadModel(mamba_config)
+        params = sum(p.numel() for p in model.parameters())
+        assert params == 236_844_910_400  # ~ 236B

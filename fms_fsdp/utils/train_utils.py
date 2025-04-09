8c59c4062cf0534acdb919cd8bdfb38ccbd98486
Garrett Goon <garrett361@gmail.com>
2025-04-09 02:19:39 +0000

    move comment

--- fms_fsdp/utils/train_utils.py
+++ fms_fsdp/utils/train_utils.py
@@ -104,10 +104,10 @@ def train(
             loss.backward()
 
         # Clipping gets complicated with advanced sharding -- see torchtitan
-        # .full_tensor() return the correct global norm
         if cfg.skip_clip:
             g_norms.append(0.0)
         else:
+            # .full_tensor() return the correct global norm
             g_norms.append(
                 torch.nn.utils.clip_grad_norm_(model.parameters(), cfg.grad_clip_thresh)
                 .full_tensor()

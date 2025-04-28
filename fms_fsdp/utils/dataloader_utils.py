f20a8da30a19f94438c510ed19395b411aedb16f
Davis Wertheimer <dww78@cornell.edu>
2025-04-28 11:56:14 -0400

    Passthrough target_doclen

--- fms_fsdp/utils/dataloader_utils.py
+++ fms_fsdp/utils/dataloader_utils.py
@@ -108,7 +108,7 @@ def get_data_loader(cfg, rank, world_size, dp_degree, postprocess=[causal_lm]):
         cfg.eos_token,
         bos_token=cfg.bos_token,
         strip_tokens=set(droplist),
-        min_length=8192,
+        min_length=cfg.target_doclen,
         seed=cfg.seed,
         filter_exp=cfg.filter_exp,
     )

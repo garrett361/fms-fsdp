ec520a456bd686be5007016e1b36f1bba065b042
Garrett Goon <goon@ibm.com>
2025-05-16 20:21:55 +0000

    test total, exp, active params

--- tests/moe/test_model.py
+++ tests/moe/test_model.py
@@ -1,5 +1,8 @@
 import torch
-from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+from mamba_ssm.models.mixer_seq_simple import (
+    MambaLMHeadModel,
+    get_total_exp_and_active_params,
+)
 
 from fms_fsdp.utils.config_utils import get_model_config
 
@@ -16,50 +19,73 @@ class TestBuildModels:
         mamba_config = get_model_config("mamba_30b_moe")
         with torch.device("meta"):
             model = MambaLMHeadModel(mamba_config)
-        params = sum(p.numel() for p in model.parameters())
-        assert params == 27_911_687_584  # ~ 27B
+        total, exp, active = get_total_exp_and_active_params(model)
+        assert total == 27_911_687_584
+        assert exp == 25_367_150_592
+        assert active == 5_715_430_816
 
     def test_mamba_120b_moe(self) -> None:
         mamba_config = get_model_config("mamba_120b_moe")
         with torch.device("meta"):
             model = MambaLMHeadModel(mamba_config)
-        params = sum(p.numel() for p in model.parameters())
-        assert params == 117_685_942_784  # ~ 117B
+        total, exp, active = get_total_exp_and_active_params(model)
+        assert total == 117_685_942_784
+        assert exp == 112_742_891_520
+        assert active == 11_989_481_984
 
     def test_mamba_236b_moe(self) -> None:
         mamba_config = get_model_config("mamba_236b_moe")
         with torch.device("meta"):
             model = MambaLMHeadModel(mamba_config)
-        params = sum(p.numel() for p in model.parameters())
-        assert params == 236_844_910_400  # ~ 236B
+        total, exp, active = get_total_exp_and_active_params(model)
+        assert total == 236_844_910_400
+        assert exp == 226_492_416_000
+        assert active == 21_677_115_200
 
     def test_mamba_105b_moe_sparse(self) -> None:
         mamba_config = get_model_config("mamba_105b_moe_sparse")
         with torch.device("meta"):
             model = MambaLMHeadModel(mamba_config)
-        params = sum(p.numel() for p in model.parameters())
-        assert params == 104_032_013_728  # ~ 104B
+        total, exp, active = get_total_exp_and_active_params(model)
+        assert total == 104_032_013_728
+        assert exp == 101_468_602_368
+        assert active == 5_734_305_184
 
     def test_ds_v2_lite(self) -> None:
         mamba_config = get_model_config("deepseek-v2-lite")
         with torch.device("meta"):
             model = MambaLMHeadModel(mamba_config)
-        params = sum(p.numel() for p in model.parameters())
-        assert params == 15_788_259_328  # ~ 16B
+        total, exp, active = get_total_exp_and_active_params(model)
+        assert total == 15_788_259_328  # (NOTE: @goon - should be 16B per HF)
+        assert exp == 14_394_851_328
+        assert active == 2_742_925_312  # (NOTE: @goon - should be 2.4B per HF)
 
     def test_ds_v2(self) -> None:
         mamba_config = get_model_config("deepseek-v2")
         with torch.device("meta"):
             model = MambaLMHeadModel(mamba_config)
-        params = sum(p.numel() for p in model.parameters())
-        assert params == 246_920_442_880  # ~ 246B (NOTE: @goon - should be 236B per HF)
+        total, exp, active = get_total_exp_and_active_params(model)
+        assert total == 246_920_442_880  # ~ 246B (NOTE: @goon - should be 236B per HF)
+        assert exp == 222_717_542_400
+        assert active == 32_554_808_320  # ~15B (NOTE: @goon - should be 21B per HF)
 
     def test_ds_v3(self) -> None:
         mamba_config = get_model_config("deepseek-v3")
         with torch.device("meta"):
             model = MambaLMHeadModel(mamba_config)
-        params = sum(p.numel() for p in model.parameters())
-        assert params == 688_268_357_120  # ~688 B (NOTE: @goon - should be 671B per HF)
+        total, exp, active = get_total_exp_and_active_params(model)
+        assert total == 688_268_357_120  # ~688 B (NOTE: @goon - should be 671B per HF)
+        assert exp == 653_908_770_816
+        assert active == 54_794_235_392  # ~52B (NOTE: @goon - should be ~37B?)
+
+    def test_llama4_maverick(self) -> None:
+        mamba_config = get_model_config("llama4-maverick")
+        with torch.device("meta"):
+            model = MambaLMHeadModel(mamba_config)
+        total, exp, active = get_total_exp_and_active_params(model)
+        assert total == 398_195_266_560  # ~400B
+        assert exp == 386_547_056_640
+        assert active == 14_668_108_800  # ~15B (NOTE: @goon - should be ~17B per HF)
 
     def test_mamba_dev_cfg(self) -> None:
         n_layers = 3
@@ -92,6 +118,15 @@ class TestBuildModels:
     def test_ds_v3_dev_cfg(self) -> None:
         n_layers = 3
         mamba_config = get_model_config(f"deepseek-v3-dev_{n_layers}_layer")
+        with torch.device("meta"):
+            model = MambaLMHeadModel(mamba_config)
+        assert len(model.backbone.layers) == n_layers
+        total, exp, active = get_total_exp_and_active_params(model)
+        total
+
+    def test_llama4_maverick_dev_cfg(self) -> None:
+        n_layers = 3
+        mamba_config = get_model_config(f"llama4-maverick-dev_{n_layers}_layer")
         with torch.device("meta"):
             model = MambaLMHeadModel(mamba_config)
         assert len(model.backbone.layers) == n_layers

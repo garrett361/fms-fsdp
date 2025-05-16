e3e1c53cc35b9aca488e2529f57a24875d9efe2b
Garrett Goon <goon@ibm.com>
2025-05-16 15:11:31 +0000

    llama4 maverick cfg placeholder

--- fms_fsdp/utils/config_utils.py
+++ fms_fsdp/utils/config_utils.py
@@ -470,6 +470,8 @@ def get_model_config(model_variant) -> LLaMAConfig | MambaConfig:
         model_config = DS2_CFG
     elif model_variant == "deepseek-v3":
         model_config = DS3_CFG
+    elif model_variant == "llama4-maverick":
+        raise NotImplementedError("Need to find cfg")
     elif mamba_moe_dev_config := re.search(
         r"mamba_moe_dev_(\d+)_layer_(\d+)_exp_(\d+)_act", model_variant
     ):

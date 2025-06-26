273a6eca9a5ed1a8a07fca1e1e568f7e1b27d8e9
Garrett Goon <goon@ibm.com>
2025-06-26 14:03:57 -0400

    minor edits

--- tests/sft/tokenize_dataset.py
+++ tests/sft/tokenize_dataset.py
@@ -5,27 +5,28 @@ from transformers import AutoTokenizer
 
 from fms_fsdp.utils.dataset_utils import CHAT_TEMPLATES, encode_sft_example
 
-tokenizer = AutoTokenizer.from_pretrained("/datasets/tokenizers/llama3")
-tokenizer.chat_template = CHAT_TEMPLATES["tulu"]
-print("LOAD DATA")
-train_dataset = load_dataset(
-    "parquet",
-    data_dir="/datasets/long_context_sft/longcontext_121824_cleaned_v1",
-    num_proc=4,
-)["train"]
-print("MAP DATA")
-train_dataset = train_dataset.map(
-    partial(
-        encode_sft_example,
-        tokenizer=tokenizer,
-        max_seq_length=None,
-    ),
-    batched=False,
-    num_proc=48,
-    remove_columns=[
-        name
-        for name in train_dataset.column_names
-        if name not in ["input_ids", "labels"]
-    ],
-    desc="Tokenizing and reformatting instruction data",
-)
+if __name__ == "__main__":
+    tokenizer = AutoTokenizer.from_pretrained("/datasets/tokenizers/llama3")
+    tokenizer.chat_template = CHAT_TEMPLATES["tulu"]
+    print("LOAD DATA")
+    train_dataset = load_dataset(
+        "parquet",
+        data_dir="/datasets/long_context_sft/longcontext_121824_cleaned_v1",
+        num_proc=4,
+    )["train"]
+    print("MAP DATA")
+    train_dataset = train_dataset.map(
+        partial(
+            encode_sft_example,
+            tokenizer=tokenizer,
+            max_seq_length=None,
+        ),
+        batched=False,
+        num_proc=48,
+        remove_columns=[
+            name
+            for name in train_dataset.column_names
+            if name not in ["input_ids", "labels"]
+        ],
+        desc="Tokenizing and reformatting instruction data",
+    )

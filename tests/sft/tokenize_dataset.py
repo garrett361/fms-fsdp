78428114ddfb2fd9cfe2e4dbee693ab23827f8f5
Garrett Goon <goon@ibm.com>
2025-07-06 17:28:13 -0400

    tokenize_dataset.py args

--- tests/sft/tokenize_dataset.py
+++ tests/sft/tokenize_dataset.py
@@ -1,3 +1,5 @@
+import argparse
+import multiprocessing
 from functools import partial
 
 from datasets import load_dataset
@@ -6,13 +8,25 @@ from transformers import AutoTokenizer
 from fms_fsdp.utils.dataset_utils import CHAT_TEMPLATES, encode_sft_example
 
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained("/datasets/tokenizers/llama3")
+    cpu_count = multiprocessing.cpu_count()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_proc_load", type=int, default=cpu_count // 2)
+    parser.add_argument("--num_proc_map", type=int, default=cpu_count // 2)
+    parser.add_argument("--data_dir", type=str, default="/datasets/tokenizers/llama3")
+    parser.add_argument(
+        "--tokenizer_path",
+        type=str,
+        default="/datasets/long_context_sft/longcontext_121824_cleaned_v1",
+    )
+    args = parser.parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
     tokenizer.chat_template = CHAT_TEMPLATES["tulu"]
     print("LOAD DATA")
     train_dataset = load_dataset(
         "parquet",
-        data_dir="/datasets/long_context_sft/longcontext_121824_cleaned_v1",
-        num_proc=4,
+        data_dir=args.data_dir,
+        num_proc=args.num_proc_load,
     )["train"]
     print("MAP DATA")
     train_dataset = train_dataset.map(
@@ -22,7 +36,7 @@ if __name__ == "__main__":
             max_seq_length=None,
         ),
         batched=False,
-        num_proc=48,
+        num_proc=args.num_proc_map,
         remove_columns=[
             name
             for name in train_dataset.column_names

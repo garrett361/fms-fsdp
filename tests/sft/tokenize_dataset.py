5a7f143ac63384c332fde4de5cd8cb59fdb040cd
Garrett Goon <goon@ibm.com>
2025-07-06 21:52:19 -0400

    rm tokenize_dataset.py defaults

--- tests/sft/tokenize_dataset.py
+++ tests/sft/tokenize_dataset.py
@@ -12,14 +12,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_proc_load", type=int, default=cpu_count // 2)
     parser.add_argument("--num_proc_map", type=int, default=cpu_count // 2)
-    parser.add_argument(
-        "--tokenizer_path", type=str, default="/datasets/tokenizers/llama3"
-    )
-    parser.add_argument(
-        "--data_dir",
-        type=str,
-        default="/datasets/long_context_sft/longcontext_121824_cleaned_v1",
-    )
+    parser.add_argument("--tokenizer_path", type=str)
+    parser.add_argument("--data_dir", type=str)
     parser.add_argument(
         "--save_path",
         type=str,

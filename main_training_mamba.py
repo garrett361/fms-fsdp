73516e9af4a735afcd6b58f5b36c848a1141e825
Garrett Goon <goon@ibm.com>
2025-07-17 08:36:12 -0400

    rank fixes

--- main_training_mamba.py
+++ main_training_mamba.py
@@ -99,11 +99,12 @@ def main(**kwargs):
         cp_degree = cfg.cp_degree or torch.cuda.device_count()
         if cp_degree == world_size:
             cp_mesh = get_1D_world_mesh(world_size, prefix="cp_")
+            dp_rank = 0
         else:
             cp_mesh_2d = get_2D_world_mesh(world_size, cp_degree, prefix="cp_")
-            cp_rank = cp_mesh_2d["cp_inner"].get_local_rank()
             dp_rank = cp_mesh_2d["dp_outer"].get_local_rank()
             cp_mesh = cp_mesh_2d["cp_inner"]
+        cp_rank = cp_mesh.get_local_rank()
     else:
         cp_mesh = None
         cp_degree = 1
